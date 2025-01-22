f1adec9b86c27ab29c9b288a112ab7cd4e8e105e
Martin <martinqt456@gmail.com>
2025-01-21 16:02:29 -0800

    Initial commit

---
+++ aes_fsm.srcs/sim_1/new/tb_uart_frame_rx.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 01/21/2025 03:56:11 PM
+// Design Name: 
+// Module Name: tb_uart_frame_rx
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tb_uart_frame_rx(
+
+    );
+endmodule
